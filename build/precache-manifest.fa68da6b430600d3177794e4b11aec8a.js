6a99fb5dc8c281ef7ed2e101da05568692869af2
bogdan <bshelomanov98@mail.ru>
2020-04-25 17:56:34 +0300

    create build

---
+++ build/precache-manifest.fa68da6b430600d3177794e4b11aec8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2105b71e0dc22d3ed6fa0780308c8888",
+    "url": "/algorithms/index.html"
+  },
+  {
+    "revision": "6bb109b023fb7b0001b1",
+    "url": "/algorithms/static/css/main.5c0a7a55.chunk.css"
+  },
+  {
+    "revision": "4be7e6cbde6f2ef00bf6",
+    "url": "/algorithms/static/js/2.8c9b247a.chunk.js"
+  },
+  {
+    "revision": "6bb109b023fb7b0001b1",
+    "url": "/algorithms/static/js/main.ab4346fe.chunk.js"
+  },
+  {
+    "revision": "9c1989cdcc0ec1573ba5",
+    "url": "/algorithms/static/js/runtime~main.17b63176.js"
+  }
+]);
