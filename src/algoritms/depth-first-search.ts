e7c035926d99aa73387827376bf8b16e498f8928
bogdan <bshelomanov98@mail.ru>
2020-06-04 16:25:35 +0300

    go to ts (refactor dfs algoritm)

--- src/algoritms/depth-first-search.ts
+++ src/algoritms/depth-first-search.ts
@@ -1,17 +1,29 @@
+import { GraphType, Graph } from "graph";
 import { canVisitedVertex, restorePath } from "./utils";
 import { AlgoritmController } from "./controller";
 
-export function depthFirstSearch(startIndex, endIndex, graph, graphControll) {
+export function depthFirstSearch(
+  startIndex: number,
+  endIndex: number,
+  graph: GraphType,
+  graphControll: Graph
+) {
+  let prevIndex = null;
+  let isWork = true;
+
   const aInfo = new AlgoritmController(startIndex, endIndex);
 
-  let isWork = true;
   const stack = [startIndex];
-  const visited = [];
-  const parent = {};
-  let prevIndex = null;
+  const visited = [startIndex];
+  const path: { [key: string]: number } = {};
 
   while (isWork && stack.length > 0) {
     const currentIndex = stack.shift();
+
+    if (!currentIndex) {
+      isWork = false;
+      break;
+    }
 
     aInfo.addVertex(
       {
@@ -22,30 +34,41 @@ export function depthFirstSearch(startIndex, endIndex, graph, graphControll) {
     );
 
     for (let i = 0; i < graph[currentIndex].siblings.length; i++) {
-      const next = graph[currentIndex].siblings[i];
-      const vertex = graphControll.getVertexByIndex(next.vertex);
+      const sibling = graph[currentIndex].siblings[i];
 
-      if (!visited.includes(next.vertex) && canVisitedVertex(vertex)) {
-        stack.unshift(next.vertex);
-        visited.push(next.vertex);
+      if (!sibling) {
+        isWork = false;
+        break;
+      }
 
-        parent[next.vertex] = currentIndex;
+      const vertex = graphControll.getVertexByIndex(sibling.vertex);
+
+      if (
+        vertex &&
+        !visited.includes(sibling.vertex) &&
+        canVisitedVertex(vertex)
+      ) {
+        stack.unshift(sibling.vertex);
+        visited.push(sibling.vertex);
+
+        path[sibling.vertex] = currentIndex;
         aInfo.increment();
       }
 
-      if (next.vertex === endIndex) {
+      if (sibling.vertex === endIndex) {
         isWork = false;
         break;
       }
     }
+
     prevIndex = currentIndex;
   }
 
   const result = aInfo.getAlgotitmResult();
-  const path = restorePath(endIndex, startIndex, parent);
+  const restoredPath = restorePath(endIndex, startIndex, path);
 
   return {
-    path,
     ...result,
+    path: restoredPath,
   };
 }
